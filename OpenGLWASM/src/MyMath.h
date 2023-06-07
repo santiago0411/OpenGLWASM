e83df83d9127378187c24580ddb39b98d483d5bc
santiago0411 <49250130+santiago0411@users.noreply.github.com>
2023-06-07 15:32:58 -0300

    Changed in Mat4 ptrs to const (header didnt get commited)

--- OpenGLWASM/src/MyMath.h
+++ OpenGLWASM/src/MyMath.h
@@ -12,5 +12,5 @@ typedef struct  {
 void Mat4Init(Mat4* mat, const float s);
 void Mat4Identity(Mat4* mat);
 float* Mat4ValuePtr(Mat4* mat);
-void Mat4Multiply(Mat4* m1, Mat4* m2, Mat4* outResult);
-void MatRotate(Mat4* mat, float angle, const Vec3* rotation, Mat4* outResult);
+void Mat4Multiply(const Mat4* m1, const Mat4* m2, Mat4* outResult);
+void MatRotate(const Mat4* mat, float angle, const Vec3* rotation, Mat4* outResult);
